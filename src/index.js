6933fefebcfaa0f3e17d6f5f2e38b8fbfa8093aa
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2020-03-28 04:43:43 +0100

    init push

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./views/LandingPage/LandingPage";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {main: '#000000'},
+        secondary: {main: '#757575'},
+        // blue: {main: '#1a73e8'},
+    },
+    typography: {useNextVariants: true},
+});
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/test" component={App} />
+            <Route path="/" component={LandingPage} />
+        </Switch>
+    </BrowserRouter>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/views/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FreeSoloCreateOption from "../../components/Autocomplete/Autocomplete";
+import './LandingPage.css';
+
+
+export default function LandingPage(props) {
+    return (
+        <div>
+            <header className="lp-header">
+                <FreeSoloCreateOption/>
+            </header>
+
+        </div>
+    );
+}
