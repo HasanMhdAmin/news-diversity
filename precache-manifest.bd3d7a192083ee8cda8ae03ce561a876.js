c45eae3bc76963d893c14a2defa5a69e03df1717
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2020-06-06 10:47:21 +0200

    Updates

---
+++ precache-manifest.bd3d7a192083ee8cda8ae03ce561a876.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4480b40e3aa5b25e70a26f71d7ef7d",
+    "url": "/news-diversity/index.html"
+  },
+  {
+    "revision": "a754c9b5727c70829841",
+    "url": "/news-diversity/static/css/2.6b8b0033.chunk.css"
+  },
+  {
+    "revision": "f861a574fec2123ee670",
+    "url": "/news-diversity/static/css/main.070578b3.chunk.css"
+  },
+  {
+    "revision": "a754c9b5727c70829841",
+    "url": "/news-diversity/static/js/2.8e9a4ff5.chunk.js"
+  },
+  {
+    "revision": "8cc2b7a5b3876cf6863364c6f4f09d96",
+    "url": "/news-diversity/static/js/2.8e9a4ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f861a574fec2123ee670",
+    "url": "/news-diversity/static/js/main.b18a2dca.chunk.js"
+  },
+  {
+    "revision": "09aece5b6cb903f56db0",
+    "url": "/news-diversity/static/js/runtime-main.c7247b97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-diversity/static/media/logo.5d5d9eef.svg"
+  }
+]);
